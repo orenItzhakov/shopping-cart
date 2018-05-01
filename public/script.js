2bf14724b390cb0c91a05020a7d9b958489700f9
unknown <oren3274@gmail.com>
2018-05-01 10:07:09 +0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,69 @@
+var ShoppingCart = function () {
+
+  // an array with all of our cart items
+  var cart = [];
+
+  var updateCart = function () {
+    $(".cart-list").empty();
+    var source = $('#cart-template').html();
+    var template = Handlebars.compile(source);
+    var sum = 0;
+    Handlebars.registerHelper('mulItems', function(param1, param2){
+      sum += param1 * param2;
+      return param1 * param2;
+    });
+    var newHTML = template({items : cart});
+    $('.cart-list').append(newHTML);
+    $(".total").text(sum);
+  }
+
+
+  var addItem = function (item,price) {
+    flag = true;
+    for (var i = 0; i < cart.length; i++) {
+      if(cart[i].name == item){
+        cart[i].count += 1;
+        flag = false;
+      }
+    }
+    if (flag) cart.push({
+      name : item ,
+      price : price ,
+      count : 1
+    });
+  }
+
+  var clearCart = function () {
+    cart=[];
+  }
+
+  return {
+    updateCart: updateCart,
+    addItem: addItem,
+    clearCart: clearCart
+  }
+};
+
+var app = ShoppingCart();
+
+// update the cart as soon as the page loads!
+app.updateCart();
+
+
+//--------EVENTS---------
+
+$('.view-cart').on('click', function () {
+  $(".shopping-cart").toggleClass("show");
+});
+
+$('.add-to-cart').on('click', function () {
+  var item = $(this).closest(".item").data().name;
+  var price = $(this).closest(".item").data().price;
+  app.addItem(item,price);
+  app.updateCart();
+});
+
+$('.clear-cart').on('click', function () {
+  app.clearCart();
+  app.updateCart();
+});
